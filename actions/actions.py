a2387f618c1f0fa7a2bdff0fba138467cf1f978f
paulo_roberto_almeida <paulo.cc03@gmail.com>
2020-04-15 18:31:49 +0700

    fetch user profile first custom action

--- actions/actions.py
+++ actions/actions.py
@@ -2,6 +2,10 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+from rasa_sdk.actions import Action
+from rasa_sdk.events import SlotSet
+import requests
+
 class ActionHelloWorld(Action):
     def name(self) -> Text:
         return "action_hello_world"
@@ -13,3 +17,14 @@ class ActionHelloWorld(Action):
         dispatcher.utter_message("Hello World!")
 
         return []
+
+
+
+class FetchProfileAction(Action):
+    def name(self):
+        return "fetch_profile"
+
+    def run(self, dispatcher, tracker, domain):
+        url = "http://myprofileurl.com"
+        data = requests.get(url).json
+        return [SlotSet("account_type", data["account_type"])]
